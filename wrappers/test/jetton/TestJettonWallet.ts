43842ba080cb854e2df2c0a5143fb25502ec92f2
opcc <opcc22059@gmail.com>
2024-09-18 22:29:52 +0800

    add wrapper for test jetton wallet

---
+++ wrappers/test/jetton/TestJettonWallet.ts
@@ -0,0 +1,41 @@
+import { Contract, ContractProvider, Sender, Address, Cell, contractAddress, beginCell, Slice, TupleItemSlice, TupleItemInt, Dictionary } from "@ton/core";
+import { encodeOffChainContent } from "../../../libs/cells";
+import { COMMON_OP_STAKE } from "../../common/opcodes";
+import { ADMIN_MINT } from "./opcode";
+import { JETTON_OP_TRANSFER } from "../../standard/opcodes";
+
+export default class TestJettonWallet implements Contract {
+
+  constructor(readonly address: Address, readonly init?: { code: Cell, data: Cell }) {}
+
+  async sendTransfer(provider: ContractProvider, via: Sender, queryId: number, jettonAmount: bigint, toAddress: Address, responseAddress: Address, value: string, fwdValue: string) {
+    const messageBody = beginCell()
+      .storeUint(JETTON_OP_TRANSFER, 32) // op 
+      .storeUint(queryId, 64) // query id
+      .storeCoins(jettonAmount)
+      .storeAddress(toAddress)
+      .storeAddress(responseAddress)
+      .storeDict(Dictionary.empty())
+      .storeCoins(BigInt(Number(fwdValue) * 1e9))
+      .storeAddress(responseAddress)
+      .endCell();
+    await provider.internal(via, {
+      value,
+      body: messageBody
+    });
+  }
+
+  async getWalletAddress(provider: ContractProvider, userAddress: Address) {
+    const { stack } = await provider.get("get_wallet_address", [
+        {
+          type: 'slice',
+          cell: 
+              beginCell()
+                  .storeAddress(userAddress)
+              .endCell()
+      } as TupleItemSlice
+    ]);
+    return stack.readAddress();
+  }
+
+}
